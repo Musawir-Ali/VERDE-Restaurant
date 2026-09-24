0ba7ff6da1bddc14f87aae0a9312a07ad03ba5db
Musawir Ali <musawirali106@gmail.com>
2026-09-24 13:12:33 +0500

    add JavaScript on Reservation

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ nextBtn.addEventListener("click", () => {
 prevBtn.addEventListener("click", () => {
     track.scrollBy({ left: -340, behavior: "smooth" });
 });
+
+const reserveForm = document.querySelector(".reserve-form");
+reserveForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const nameInput = reserveForm.querySelector('input[name="name"]');
+    const guestName = nameInput.value;
+
+    reserveForm.innerHTML = `
+        <div class="reserve-success">
+            <h3>Thank you, ${guestName}.</h3>
+            <p>We've received your request and will confirm your reservation by email.</p>
+        </div>
+    `;
+})
